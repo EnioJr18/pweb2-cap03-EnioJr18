7116db87b055f97d1290304f1d6a72ef2c0ac7aa
Enio Jr <eniojr100@gmail.com>
2026-09-17 15:43:01 -0300

    :sparkles: feat(rota-03): responde GET /saudacao/:nome personalizada

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import http from 'node:http';
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  
+
   if (req.method === 'GET' && req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
     res.end('Olá, Mundo!');
@@ -14,6 +14,15 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end('<h1>Sobre</h1>');
     return;
+  }
+
+  if (req.method === 'GET' && req.url.startsWith('/saudacao/')) {
+  const partes = req.url.split('/');
+  const nome = partes[2]; 
+  
+  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+  res.end(`Olá, ${nome}!`);
+  return;
   }
 });
 
